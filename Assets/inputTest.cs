e3ad3de12314daa92c649d55fcaeeaad3bb016be
StirlingPM <69620695+StirlingPM@users.noreply.github.com>
2021-03-27 12:57:20 +1100

    Controller input working for 2 players with both keyboard and arcade controller

--- Assets/inputTest.cs
+++ Assets/inputTest.cs
@@ -6,14 +6,14 @@ public class inputTest : MonoBehaviour
 {
     // Update is called once per frame
     ArcadeInput input;
-
+    [SerializeField] float speed;
     private void Awake()
     {
         input = GetComponent<ArcadeInput>();
     }
     void Update()
     {
-        transform.Translate(new Vector3(Input.GetAxisRaw(input.hori), 0, Input.GetAxisRaw(input.verti)) * Time.deltaTime);
+        transform.Translate(new Vector3(Input.GetAxisRaw(input.hori), 0, Input.GetAxisRaw(input.verti)) * Time.deltaTime * speed);
         if (Input.GetButtonDown(input.B1))
         {
             print("B1");
